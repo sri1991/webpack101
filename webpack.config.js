24e3af6f5bea0ac290434208c34506e818aeb2b9
Vamshi <vamshi.vshs@gmail.com>
2017-04-18 13:06:03 +0530

    initial commit for webpack 2 starter kit

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+    entry: './src/app.js',
+    output:{
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'app.bundle.js'
+    },
+    plugins: [new HtmlWebpackPlugin()]
+}
